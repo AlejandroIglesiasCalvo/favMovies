67e74d9a31318799075a3718b56885e0a882a6c7
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2020-09-23 17:41:34 +0200

    Barra de notificaciones correctamente mostrada

--- app/src/main/java/es/uniovi/eii/sdm/MainActivity.java
+++ app/src/main/java/es/uniovi/eii/sdm/MainActivity.java
@@ -3,7 +3,11 @@ package es.uniovi.eii.sdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -11,9 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Toast.makeText(getApplicationContext(),getString(R.string.OnCreate), Toast.LENGTH_SHORT).show();
+        Button btnGuardar = (Button) findViewById(R.id.button);
+        btnGuardar.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(findViewById(R.id.Layaut), R.string.msg_guardado,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }));
+
     }
 
-  /*  @Override
+    /*  @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(getApplicationContext(),getString(R.string.onStart), Toast.LENGTH_SHORT).show();
