8141b020bc589a013703a3692af41079e0cb08d9
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2020-09-30 17:24:29 +0200

    Cambio del titulo en funcion de si se crea o modifica la categoria

--- app/src/main/java/es/uniovi/eii/sdm/CategoriaActivity.java
+++ app/src/main/java/es/uniovi/eii/sdm/CategoriaActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import es.uniovi.eii.sdm.modelo.Categoria;
 
@@ -15,11 +18,28 @@ public class CategoriaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_categoria);
 
         //Recepcion de datos
-        Intent intent= getIntent();
-        int posCategoria = intent.getIntExtra(MainActivity.POS_CATEGORIA_SELECCIONADA,0);
-        Categoria categEntrada=null;
-         if(posCategoria>0){
-             categEntrada=intent.getParcelableExtra(MainActivity.POS_CATEGORIA_SELECCIONADA);
-         }
+        Intent intent = getIntent();
+        int posCategoria = intent.getIntExtra(MainActivity.POS_CATEGORIA_SELECCIONADA, 0);
+        Categoria categEntrada = null;
+        if (posCategoria > 0) {
+            categEntrada = intent.getParcelableExtra(MainActivity.POS_CATEGORIA_SELECCIONADA);
+        }
+
+        TextView textViewCrea = (TextView) findViewById(R.id.TituloCrearCategoria);
+        final EditText editNomCategoria = (EditText) findViewById(R.id.txtNombreCategoria);
+        final EditText editDescripcion = (EditText) findViewById(R.id.txtDescripcionCategoria);
+        // Recuperamos referencia al botón
+        Button btnOk = (Button) findViewById(R.id.btnOK);
+        Button btnCancel = (Button) findViewById(R.id.btnCancelar);
+//cambiamos la etiqueta del titulo en funcion de si crea o modifica
+        if (posCategoria == 0) {
+            textViewCrea.setText(R.string.TituloCrearCategoria);
+        } else {
+            textViewCrea.setText(R.string.TituloModificarCategoria);
+            editNomCategoria.setText(categEntrada.getNombre());
+            editDescripcion.setText(categEntrada.getDescripcion());
+            editNomCategoria.setEnabled(false);
+        }
+
     }
 }
