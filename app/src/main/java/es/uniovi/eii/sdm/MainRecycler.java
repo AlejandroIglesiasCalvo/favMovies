0e310233f4690852f83502760fba689df4c257a6
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2020-10-07 16:35:18 +0200

    Clase pelicula, aprcelable

--- app/src/main/java/es/uniovi/eii/sdm/MainRecycler.java
+++ app/src/main/java/es/uniovi/eii/sdm/MainRecycler.java
@@ -1,14 +1,26 @@
 package es.uniovi.eii.sdm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainRecycler extends AppCompatActivity {
-
+ RecyclerView listaPeliView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_recycler);
+
+        listaPeliView = (RecyclerView)findViewById(R.id.recycler);
+        listaPeliView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        listaPeliView.setLayoutManager(layoutManager);
+
+
+
+
     }
 }
